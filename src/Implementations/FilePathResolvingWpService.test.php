510576afa4f8d37d4fa4d9b0d62916d7eb1a58e2
Sebastian Thulin <sebastianthulin@users.noreply.github.com>
2024-11-11 13:47:03 +0100

    fix: add wp prefix, align with type definition (#58)

--- src/Implementations/FilePathResolvingWpService.test.php
+++ src/Implementations/FilePathResolvingWpService.test.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
 class FilePathResolvingWpServiceTest extends TestCase
 {
     /**
-     * @testdox registerScript() calls the inner service with the resolved file path
+     * @testdox wpRegisterScript() calls the inner service with the resolved file path
      */
     public function testRegisterScript()
     {
@@ -17,14 +17,14 @@ class FilePathResolvingWpServiceTest extends TestCase
         $filePathResolver = $this->getFilePathResolver();
 
         $filePathResolvingWpService = new FilePathResolvingWpService($innerService, $filePathResolver);
-        $filePathResolvingWpService->registerScript('handle', 'path/to/script.js');
+        $filePathResolvingWpService->wpRegisterScript('handle', 'path/to/script.js');
 
-        $this->assertEquals('handle', $innerService->invocations['registerScript'][0]);
-        $this->assertEquals('resolved: path/to/script.js', $innerService->invocations['registerScript'][1]);
+        $this->assertEquals('handle', $innerService->invocations['wpRegisterScript'][0]);
+        $this->assertEquals('resolved: path/to/script.js', $innerService->invocations['wpRegisterScript'][1]);
     }
 
     /**
-     * @testdox registerStyle() calls the inner service with the resolved file path
+     * @testdox wpRegisterStyle() calls the inner service with the resolved file path
      */
     public function testRegisterStyle()
     {
@@ -32,10 +32,10 @@ class FilePathResolvingWpServiceTest extends TestCase
         $filePathResolver = $this->getFilePathResolver();
 
         $filePathResolvingWpService = new FilePathResolvingWpService($innerService, $filePathResolver);
-        $filePathResolvingWpService->registerStyle('handle', 'path/to/stylesheet.css');
+        $filePathResolvingWpService->wpRegisterStyle('handle', 'path/to/stylesheet.css');
 
-        $this->assertEquals('handle', $innerService->invocations['registerStyle'][0]);
-        $this->assertEquals('resolved: path/to/stylesheet.css', $innerService->invocations['registerStyle'][1]);
+        $this->assertEquals('handle', $innerService->invocations['wpRegisterStyle'][0]);
+        $this->assertEquals('resolved: path/to/stylesheet.css', $innerService->invocations['wpRegisterStyle'][1]);
     }
 
     private function getInnerService(): WpService
@@ -43,24 +43,26 @@ class FilePathResolvingWpServiceTest extends TestCase
         return new class extends NativeWpService {
             public array $invocations = [];
 
-            public function registerStyle(
+            public function wpRegisterStyle(
                 string $handle,
-                string $src = '',
+                string|false $src,
                 array $deps = array(),
                 string|bool|null $ver = false,
                 string $media = 'all'
-            ): void {
-                $this->invocations['registerStyle'] = func_get_args();
+            ): bool {
+                $this->invocations['wpRegisterStyle'] = func_get_args();
+                return true;
             }
 
-            public function registerScript(
+            public function wpRegisterScript(
                 string $handle,
-                string $src = '',
+                string|false $src,
                 array $deps = array(),
                 string|bool|null $ver = false,
-                bool $in_footer = true
-            ): void {
-                $this->invocations['registerScript'] = func_get_args();
+                array|bool $args = array()
+            ): bool {
+                $this->invocations['wpRegisterScript'] = func_get_args();
+                return true;
             }
         };
     }
